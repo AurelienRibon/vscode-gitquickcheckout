a6e2bfb8a7133673870463d71008c57b1b4bcf5c
Aurelien Ribon <aurelien.ribon@gmail.com>
2020-11-04 21:42:19 +0100

    Simplify code even more, no need to map refs since we checkout ref names

--- src/lib/git-utils.ts
+++ src/lib/git-utils.ts
@@ -4,57 +4,50 @@ import * as vscode from 'vscode';
 import * as vsUtils from './vs-utils';
 import { GitExtension, Ref, RefType, Repository } from '../types/git';
 
-type RefMap = Map<string, Ref>;
+type RefMap = Map<string, Repository[]>;
 
 interface GitContext {
+  repos: Repository[];
   refNames: string[];
-  refNamesTree: Map<string, RefMap>;
+  refNamesMap: RefMap;
 }
 
 export function listRefNames(): GitContext {
   const repos = getRepos();
-  const refNamesMaps = repos.map(mapRepoRefNames);
-  const refNames = extractUniqueRefNames(refNamesMaps);
-  const refNamesTree = new Map(repos.map(getRepoPath).map((it, i) => [it, refNamesMaps[i]]));
-  return { refNames, refNamesTree };
+  const refNamesMap = new Map() as RefMap;
+  repos.forEach((it) => registerRefNames(it, refNamesMap));
+
+  const refNames = [...refNamesMap.keys()].sort();
+  return { repos, refNames, refNamesMap };
 }
 
 export async function checkoutRef(refName: string, context: GitContext): Promise<void> {
-  const repos = getRepos();
-  await parallelize(repos, (repo) => checkoutRepoRef(repo, refName, context));
+  const reposWithRef = getReposWithRef(refName, context);
+  await parallelize(reposWithRef, (repo) => checkoutRepoRef(repo, refName));
+
+  const reposWithoutRef = getReposWithoutRef(refName, context);
+  await parallelize(reposWithoutRef, (repo) => checkoutRepoRef(repo, getDefaultRefName()));
 }
 
 // -----------------------------------------------------------------------------
-// HELPERS: GIT MUTATIONS
+// HELPERS: GIT MUTATORS
 // -----------------------------------------------------------------------------
 
-async function checkoutRepoRef(repo: Repository, refName: string, context: GitContext): Promise<void> {
-  const path = getRepoPath(repo);
-  const ref = context.refNamesTree.get(path)?.get(refName);
-  const defaultRefName = getDefaultRefName();
-
-  if (repo.state.HEAD?.name === ref?.name) {
-    return;
-  }
-
-  console.log(repo.rootUri.fsPath, refName, ref);
-
-  if (!ref) {
-    if (refName !== defaultRefName) {
-      await checkoutRepoRef(repo, defaultRefName, context);
-    }
+async function checkoutRepoRef(repo: Repository, refName: string): Promise<void> {
+  if (refName === simplifyRefName(repo.state.HEAD)) {
     return;
   }
 
   try {
     await repo.checkout(refName);
   } catch (err) {
+    const path = repo.rootUri.fsPath;
     console.error(`Failed to checkout ref in folder ${path}. ${err.message}`);
   }
 }
 
 // -----------------------------------------------------------------------------
-// HELPERS: GIT ACCESS
+// HELPERS: GIT GETTERS
 // -----------------------------------------------------------------------------
 
 function getRepos(): Repository[] {
@@ -63,8 +56,13 @@ function getRepos(): Repository[] {
   return api ? api.repositories : [];
 }
 
-function getRepoPath(repo: Repository): string {
-  return repo.rootUri.fsPath;
+function getReposWithRef(refName: string, context: GitContext): Repository[] {
+  return context.refNamesMap.get(refName) || [];
+}
+
+function getReposWithoutRef(refName: string, context: GitContext): Repository[] {
+  const reposWithRef = new Set(context.refNamesMap.get(refName));
+  return context.repos.filter((it) => !reposWithRef.has(it));
 }
 
 function getDefaultRefName(): string {
@@ -75,33 +73,26 @@ function getDefaultRefName(): string {
 // HELPERS: REF MAPPING
 // -----------------------------------------------------------------------------
 
-function mapRepoRefNames(repo: Repository): RefMap {
-  const map: RefMap = new Map();
+function registerRefNames(repo: Repository, map: RefMap): void {
+  const refNames = repo.state.refs.map(simplifyRefName).filter(isString);
 
-  for (const ref of repo.state.refs) {
-    const simpleRefName = simplifyRefName(ref);
-
-    // If the ref does not interest us, carry on.
-    if (!simpleRefName) {
-      continue;
+  for (const refName of refNames) {
+    let bucket = map.get(refName);
+    if (!bucket) {
+      bucket = [];
+      map.set(refName, bucket);
     }
 
-    // If we already saw this ref, we let it be overridden if the new ref is a
-    // local one. It's an opinionated choice.
-    if (ref.type !== RefType.Head && map.has(simpleRefName)) {
-      continue;
-    }
-
-    map.set(simpleRefName, ref);
+    bucket.push(repo);
   }
-
-  return map;
 }
 
-function simplifyRefName(ref: Ref) {
-  if (ref.type === RefType.Head && ref.name) {
+function simplifyRefName(ref?: Ref) {
+  if (!ref || !ref.name) {
+    return undefined;
+  } else if (ref.type === RefType.Head) {
     return ref.name;
-  } else if (ref.type === RefType.RemoteHead && ref.name && ref.remote) {
+  } else if (ref.type === RefType.RemoteHead && ref.remote) {
     return ref.name?.slice(ref.remote.length + 1);
   }
 }
@@ -115,14 +106,6 @@ async function parallelize<T>(repositories: Repository[], fn: (repo: Repository)
   return Promise.all(promises);
 }
 
-function extractUniqueRefNames(maps: RefMap[]): string[] {
-  const set = new Set<string>();
-
-  for (const map of maps) {
-    for (const key of map.keys()) {
-      set.add(key);
-    }
-  }
-
-  return [...set].sort();
+function isString(value: unknown): value is string {
+  return typeof value === 'string';
 }
